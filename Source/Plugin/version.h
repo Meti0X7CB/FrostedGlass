1f26de733c5dcfe8772a9fb107648ca9ab98f19f
Mehdi Eftekhari <mehdieft95@gmail.com>
2023-07-05 19:23:07 -0400

    1.1.3
    Compiling and building the dll file with the added Corner option for Windows 11 by khanhas

--- Source/Plugin/version.h
+++ Source/Plugin/version.h
@@ -1,9 +1,9 @@
-#define PLUGIN_VERSION "1.1.2"
+#define PLUGIN_VERSION "1.1.3"
 #define PLUGIN_AUTHOR "theAzack9"
-#define PLUGIN_YEAR 2018
+#define PLUGIN_YEAR 2023
 // Has to be the same as the project name!
 #define PLUGIN_NAME "FrostedGlass"
 // This is the path relative to root directory (where build.bat is placed)
 #define EXAMPLE_SKIN_PATH "ExampleSkin"
 #define EXAMPLE_SKIN_NAME "Example Skin"
-#define RAINMETER_VERSION "4.0.0.0"
+#define RAINMETER_VERSION "4.5.0.0"
